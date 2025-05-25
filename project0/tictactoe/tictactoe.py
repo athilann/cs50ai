a1c8a832445ff933c9e465b0baeb80e0f58ca5a4
{Áthila Naniny Ninow} <{athilann@gmail.com}>
2025-05-25 21:14:34 +0100

    put the first X on the board

--- project0/tictactoe/tictactoe.py
+++ project0/tictactoe/tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+import copy
 
 X = "X"
 O = "O"
@@ -19,24 +20,29 @@ def initial_state():
 
 
 def player(board):
-    """
-    Returns player who has the next turn on a board.
-    """
-    raise NotImplementedError
-
+    empty_Count = sum(cell == EMPTY for row in board for cell in row)
+    if empty_Count % 2:
+        return X
+    else:
+        return O
 
 def actions(board):
-    """
-    Returns set of all possible actions (i, j) available on the board.
-    """
-    raise NotImplementedError
+    available_Actions = set()
+
+    for row in board :
+        for cell in row:
+            if cell == EMPTY:
+                available_Actions.add((board.index(row), row.index(cell)))
+    return available_Actions
 
 
 def result(board, action):
-    """
-    Returns the board that results from making move (i, j) on the board.
-    """
-    raise NotImplementedError
+    if board[action[0]][action[1]] is not EMPTY:
+        raise Exception("Invalid action")
+    new_Board = copy.deepcopy(board)
+    player_Turn = player(board)
+    new_Board[action[0]][action[1]] = player_Turn
+    return new_Board
 
 
 def winner(board):
@@ -47,11 +53,11 @@ def winner(board):
 
 
 def terminal(board):
-    """
-    Returns True if game is over, False otherwise.
-    """
-    raise NotImplementedError
-
+    empty_Count = sum(cell == EMPTY for row in board for cell in row)
+    if empty_Count == 0:
+        return True
+    else:
+        return False
 
 def utility(board):
     """
