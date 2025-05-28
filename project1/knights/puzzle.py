bb3e83cf5a213872bec6960fc4014509e337e0b6
{Áthila Naniny Ninow} <{athilann@gmail.com}>
2025-05-28 17:28:24 +0100

    Applied style50

--- project1/knights/puzzle.py
+++ project1/knights/puzzle.py
@@ -12,16 +12,16 @@ CKnave = Symbol("C is a Knave")
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    Or(And(AKnight, Not(AKnave)),And(AKnave, Not(AKnight))),
-    Or(And(And(AKnight,AKnave), AKnight),And(Not(And(AKnave,AKnight)), AKnave))
+    Or(And(AKnight, Not(AKnave)), And(AKnave, Not(AKnight))),
+    Or(And(And(AKnight, AKnave), AKnight), And(Not(And(AKnave, AKnight)), AKnave))
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    Or(And(AKnight, Not(AKnave)),And(AKnave, Not(AKnight))),
-    Or(And(BKnight, Not(BKnave)),And(BKnave, Not(BKnight))),
+    Or(And(AKnight, Not(AKnave)), And(AKnave, Not(AKnight))),
+    Or(And(BKnight, Not(BKnave)), And(BKnave, Not(BKnight))),
     Or(And(And(AKnave, BKnave), AKnight), And(Not(And(AKnave, BKnave)), AKnave))
 )
 
@@ -29,10 +29,11 @@ knowledge1 = And(
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    Or(And(AKnight, Not(AKnave)),And(AKnave, Not(AKnight))),
-    Or(And(BKnight, Not(BKnave)),And(BKnave, Not(BKnight))),
-    Or(Or(And(And(AKnave, BKnave), AKnight), And(Not(And(AKnave, BKnave)), AKnave)), Or(And(And(AKnight, BKnight), AKnight), And(Not(And(AKnight, BKnight)), AKnave))),
-    Or(And(And(BKnight,AKnave), BKnight), And(And(BKnave,AKnight), BKnight))
+    Or(And(AKnight, Not(AKnave)), And(AKnave, Not(AKnight))),
+    Or(And(BKnight, Not(BKnave)), And(BKnave, Not(BKnight))),
+    Or(Or(And(And(AKnave, BKnave), AKnight), And(Not(And(AKnave, BKnave)), AKnave)), Or(
+        And(And(AKnight, BKnight), AKnight), And(Not(And(AKnight, BKnight)), AKnave))),
+    Or(And(And(BKnight, AKnave), BKnight), And(And(BKnave, AKnight), BKnight))
 )
 
 # Puzzle 3
@@ -41,11 +42,12 @@ knowledge2 = And(
 # B says "C is a knave."
 # C says "A is a knight."
 knowledge3 = And(
-    Or(And(AKnight, Not(AKnave)),And(AKnave, Not(AKnight))),
-    Or(And(BKnight, Not(BKnave)),And(BKnave, Not(BKnight))),
-    Or(And(CKnight, Not(CKnave)),And(CKnave, Not(CKnight))),
-    And(Or(And(AKnight, Not(AKnave)),And(AKnave, Not(AKnight))), AKnight),
-    And(Or(And(AKnave, BKnight), And(Not(And(AKnave, BKnight)), BKnave)),Or(And(CKnave, BKnight), And(Not(And(CKnave, BKnight)), BKnave))),
+    Or(And(AKnight, Not(AKnave)), And(AKnave, Not(AKnight))),
+    Or(And(BKnight, Not(BKnave)), And(BKnave, Not(BKnight))),
+    Or(And(CKnight, Not(CKnave)), And(CKnave, Not(CKnight))),
+    And(Or(And(AKnight, Not(AKnave)), And(AKnave, Not(AKnight))), AKnight),
+    And(Or(And(AKnave, BKnight), And(Not(And(AKnave, BKnight)), BKnave)),
+        Or(And(CKnave, BKnight), And(Not(And(CKnave, BKnight)), BKnave))),
     Or(And(AKnight, CKnight), And(AKnave, CKnave)),
     # TODO Not sure if I sould test A for a Knave
 )
